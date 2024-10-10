684a88458694abe3d82a48cb61bc0d81665f6ae0
jzdypo <1014484163@qq.com>
2024-10-10 20:19:00 +0800

    加入了GPL开源协议和文件版权头

--- device_with_hid/get_target_dev_info.c
+++ device_with_hid/get_target_dev_info.c
@@ -1,11 +1,17 @@
-/*
- ***********************************************
-
-@Author:   zappen
-@Mail:     zp935675863@gmail.com
-@Date:     2024-08-03
-@FileName: demo.c
- ***********************************************
+/* Copyright (C) 2024 赵鹏 (Peng Zhao) <224712239@csu.edu.cn>, 王振锋 (Zhenfeng Wang) <234711103@csu.edu.cn>, 杨纪琛 (Jichen Yang) <234712186@csu.edu.cn>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <stdio.h>
